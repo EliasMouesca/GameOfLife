b84554a9a6dc7d7c75e845ff2da45f2cabb8cfe8
Eli Mouesca <elias.mouesca@gmail.com>
2026-02-16 12:36:02 -0300

    Test example

--- src/example/example_test.c
+++ src/example/example_test.c
@@ -1,5 +1,17 @@
 #include "example.h"
+#include <assert.h>
 
 int main() {
+
+    example_t example = nothing();
+
+    assert(example.rows == 0);
+    assert(example.rows == 0);
+    assert(example.hpos == LEFT);
+    assert(example.vpos == TOP);
+    assert(example.cells == NULL);
+
+    destroyExample(&example);
+
     return 0;
 }
